9ef7770245145b7a508061223400c7e8d6d2228c
Pablo Saavedra G <pablosaa@uni-bonn.de>
2021-10-20 01:44:26 +0200

    functions to read IWC and LWC added, function to convert hour fraction of day to DateTime added, function to integrate profile variables added

--- src/CloudnetTools.jl
+++ src/CloudnetTools.jl
@@ -29,6 +29,159 @@ include("arm_lidar2nc.jl")
 include("arm_kazr2nc.jl")
 include("arm_hsrl2nc.jl")
 
+
+"""
+Integration of matrix x over dims=1 skipping NaN, optionally
+the integration is perform with respect of vector dh.
+USAGE:
+
+Xt = ∫f(xi::Matrix)
+Xt = ∫f(xi::Matrix; dh::Vector)
+
+"""
+function ∫f(x::AbstractArray, dx::AbstractVector)
+    δx = Vector{eltype(x)}(undef, length(dx)) .= 0
+    δx[2:end] = dx |> diff
+    δx[1] =  δx[2:end] |> minimum
+
+    x[isnan.(x)] .= 0.0
+    
+    return sum(x.*δx, dims=1) |> vec
+end
+#----/
+
+"""
+Read time from netCDF file as fraction of the day and
+convert it to Julia DateTime format.
+USAGE:
+> nctime = convert_time2DateTime(year, month, day, hour_of_day)
+WHERE:
+* year, month and day -> Float32
+* hour_of_day -> Vector{Float32} with fraction of day [0 to 24]
+
+or
+> nctime = convert_time2DateTime(nc::NCDataset)
+WHERE:
+* nc -> NCDataset identifier for an opened netCDF file,
+* nctime -> Vector{DataTime}
+
+"""
+function convert_time2DateTime(yy::Int64, mm::Int64, dd::Int64, hr_time::AbstractVector)::Vector{DateTime}
+    hh = floor.(Int64, hr_time)
+    mi = @. mod(hr_time*60, 60)
+    ss = @. mod(mi*60, 60)
+    ms = @. mod(ss, 1)*1000
+        
+    mi = floor.(Int64, mi)
+    ss = floor.(Int64, ss)
+    ms = floor.(Int64, ms)
+        
+    return @. DateTime(yy, mm, dd, hh, mi, ss, ms)
+end
+function convert_time2DateTime(nc)::Vector{DateTime}
+    
+    yy = Int64(nc.attrib["year"])
+    mm = Int64(nc.attrib["month"])
+    dd = Int64(nc.attrib["day"])
+    hr_time = float.(nc["time"])
+
+    return convert_time2DateTime(yy, mm, dd, hr_time)
+end
+
+function readIWCFile(nfile::String; modelreso=false)
+
+    @assert isfile(nfile) error("$nfile cannot be found!")
+
+    vars_categorize = Dict(
+        :height => "height",
+        :iwc => "iwc_inc_rain",
+        :flag => "iwc_retrieval_status",
+    )
+
+    # Defining Output varaible:
+    var_output = Dict{Symbol, Any}()
+
+    NCDataset(nfile; format=:netcdf4_classic) do nc
+        var_output[:time] = convert_time2DateTime(nc)
+
+        for inkey ∈ keys(vars_categorize)
+            x = vars_categorize[inkey]
+            println(x)
+            tmp = nc[x][:,:]
+            if haskey(nc[x].attrib, "missing_value")
+                miss_val = nc[x].attrib["missing_value"]
+            elseif haskey(nc[x].attrib, "_FillValue")
+                miss_val = nc[x].attrib["_FillValue"]
+            else
+                miss_val = 9.96921f36
+            end
+
+            # Cleaning missing values from variables :
+            eltype(tmp) <: AbstractFloat && (tmp[tmp .≈ miss_val] .= NaN)
+            
+            var_output[inkey] = tmp
+        end
+
+    end
+
+    # Adding computed variables:
+    # integration of iwc only for pixels with flag=1 or 2:
+    var_output[:IWV] = let tmp = var_output[:iwc]
+
+        @. tmp[!(0 < var_output[:flag] < 3)] = NaN
+        ∫f(tmp, var_output[:height])
+    end
+    
+    return var_output
+end
+
+
+function readLWCFile(nfile::String; modelreso=false)
+
+    @assert isfile(nfile) error("$nfile cannot be found!")
+
+    vars_categorize = Dict(
+        :height => "height",
+        :lwc => "lwc",
+        :flag => "lwc_retrieval_status",
+        :LWP => "lwp",
+    )
+
+    # Defining Output varaible:
+    var_output = Dict{Symbol, Any}()
+
+    NCDataset(nfile; format=:netcdf4_classic) do nc
+        var_output[:time] = convert_time2DateTime(nc)
+
+        for inkey ∈ keys(vars_categorize)
+            x = vars_categorize[inkey]
+            println(x)
+            tmp = nc[x][:,:]
+            if haskey(nc[x].attrib, "missing_value")
+                miss_val = nc[x].attrib["missing_value"]
+            elseif haskey(nc[x].attrib, "_FillValue")
+                miss_val = nc[x].attrib["_FillValue"]
+            else
+                miss_val = 9.96921f36
+            end
+
+            # Cleaning missing values from variables :
+            eltype(tmp) <: AbstractFloat && (tmp[tmp .≈ miss_val] .= NaN)
+            
+            var_output[inkey] = tmp
+        end
+
+    end
+
+    # Additional computation:
+    # integration of iwc only for pixels with flag=1 or 2:
+    @. var_output[:lwc][!(0 < var_output[:flag] < 3)] = NaN
+    
+    return var_output
+end
+# ----/
+
+
 function readCLNFile(nfile::String; modelreso=false)
     @assert isfile(nfile) error("$nfile cannot be found!")
     if contains(nfile, "categorize")
@@ -74,22 +227,8 @@ function readCLNFile(nfile::String; modelreso=false)
 
     # Starting reading CloudNet files:
     NCDataset(nfile; format=:netcdf4_classic) do nc
-        yy = Int64(nc.attrib["year"])
-        mm = Int64(nc.attrib["month"])
-        dd = Int64(nc.attrib["day"])
-        tmp_time = float.(nc["time"])
-        hh = floor.(Int64, tmp_time)
-        mi = @. mod(tmp_time*60, 60)
-        ss = @. mod(mi*60, 60)
-        ms = @. mod(ss, 1)*1000
-        
-        mi = floor.(Int64, mi)
-        ss = floor.(Int64, ss)
-        ms = floor.(Int64, ms)
-        
-        tit = @. DateTime(yy, mm, dd, hh, mi, ss, ms)
 
-        var_output[:time] = tit
+        var_output[:time] = convert_time2DateTime(nc)
 
         var_output[:height] = nc["height"][:]
 
