ff7a0ef5d32fc0aa5fc8f378370c1edb7a1a31c2
Pablo Saavedra Garfias <pablosaa@uni-bonn.de>
2022-08-01 14:00:32 +0200

    merging ACTRIS and QC modules into the first

--- src/Cloudnet_ACTRIS.jl
+++ src/Cloudnet_ACTRIS.jl
@@ -9,6 +9,7 @@
 =#
 module ACTRIS
 
+using DataFrames
 using PyCall
 
 catcnet = pyimport("cloudnetpy.categorize")
@@ -114,8 +115,8 @@ function generate_products(clnet_file::Dict, CLNTprod::Dict)
 
 	clnet_file[:categorize] = try
 	    replace(clnet_file[K], String(K)=>"categorize")
-	catch
-	    @warn "cannot generage categorize file name"
+	catch e
+	    @warn "cannot generage categorize file name: $e"
 	    "nothing"
 	end
     end
@@ -136,7 +137,6 @@ function generate_products(clnet_file::Dict, CLNTprod::Dict)
 end
 # ----/
 
-end # --- end of module
 # ================================================================
 
 #=
@@ -145,28 +145,29 @@ end # --- end of module
  ++++ MODULE QC for checking generated arm input data files +++++++
  ******************************************************************
 =#
-module QC
 
-using DataFrames
-using PyCall
+cloudnetpy_qc = pyimport("cloudnetpy_qc")
 
-cloudnet_qc = pyimport("cloudnetpy_qc")
-
-function get_cloudnet_qc(data_file::Dict)
+function cloudnet_qc(data_file::Dict)
 
     meta_df = DataFrame()
     data_df = DataFrame()
     foreach(data_file) do (k, fn)
         println("***** ", k, ":")
-        tmp = get_cloudnet_qc(fn)
+        tmp = cloudnet_qc(fn)
         #meta_df = vcat(meta_df, hcat(DataFrame(:type=>k), tmp[:metadata_qc]))
         #data_df = vcat(data_df, hcat(DataFrame(:type=>k), tmp[:data_qc]))
     end
     return #meta_df, data_df
 end
-function get_cloudnet_qc(fn::String)
+function cloudnet_qc(fn::String)
+
+    function Dict2DF(dict_in::Dict)
+        return DataFrame(; [Symbol(k)=>v for (k,v) ∈ dict_in if !isempty(v)]...)
+    end
     
-    quality = cloudnet_qc.Quality(fn)
+    quality = cloudnetpy_qc.Quality(fn)
+    
     metadata_results = quality.check_metadata()
     data_results = quality.check_data()
     println("METADATA_QC:")
@@ -175,10 +176,6 @@ function get_cloudnet_qc(fn::String)
     println(Dict2DF(data_results))
     return Dict(:metadata_qc => Dict2DF(metadata_results),
                 :data_qc => Dict2DF(data_results))
-end
-
-function Dict2DF(dict_in::Dict)
-    return DataFrame(; [Symbol(k)=>v for (k,v) ∈ dict_in if !isempty(v)]...)
 end
 # ----/
 
